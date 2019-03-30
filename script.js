f4a03ef52af5622132d06e5d0f4c7bcd533eb5e4
CorentinFoucrier <foucrier.corentin@gmail.com>
2019-03-30 11:46:56 +0100

    Debut des opération !

---
+++ script.js
@@ -0,0 +1,23 @@
+var input = '';
+var nbResult = '';
+
+function affichage(input) {
+	document.getElementById('affichage').innerHTML = input;
+}
+
+function nbPad(nb) {
+	affichage(input); //replace 0 par rien
+	input+=nb;
+	affichage(input); //ajout nb en strig à input
+}
+
+function operator(optr) {
+	affichage(input);
+	input = input + ' ' +optr+ ' ';
+	affichage(input);
+}
+
+function result() {
+	nbResult = eval(input);
+	document.getElementById('affichage').innerHTML = nbResult;
+}
